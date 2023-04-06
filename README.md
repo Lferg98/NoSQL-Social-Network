9adefb0627a1c20f85176268ecadb8320d407172
Lferg98 <lachieferg98@hotmail.com>
2023-04-06 20:48:52 +1000

    added server.js & readme layout

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # NoSQL-Social-Network
 
-Module 18 Homework : 
+Module 18 Homework @ Monash Uni Coding Bootcamp.
+
+## Table of Contents
+
+[Description](#description)
+[Installation](#installation)
+[Technologies](#technolgoies)
+[Packages](#packages)
+[Screenshot](#screenshot)
+[Walkthrough](#walkthrough)
+[Questions](#questions)
+
+## Description
+
+## Installation
+
+## Technologies
+
+## Packages 
+
+## Screenshot
+
+## Walkthrough
+
+## Questions
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/NoSQL-Social-Network', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true  
+});
+
+// log mongoose queries with the following
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log (` Connected on localhost:${PORT}`));
